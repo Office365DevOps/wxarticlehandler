a6deba82b52388d835c729a63f2698704283a586
Ares Chen <arechen@microsoft.com>
2019-12-20 13:01:13 +0800

    添加项目文件。

---
+++ WXArticleHandler/Bots/EchoBot.cs
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.6.2
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+
+namespace WXArticleHandler.Bots
+{
+    public class EchoBot : TeamsActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"Echo: {turnContext.Activity.Text}"), cancellationToken);
+        }
+
+        protected override async Task<MessagingExtensionResponse> OnTeamsAppBasedLinkQueryAsync(ITurnContext<IInvokeActivity> turnContext, AppBasedLinkQuery query, CancellationToken cancellationToken)
+        {
+            //You'll use the query.link value to search your service and create a card response
+            var heroCard = new ThumbnailCard
+            {
+                Title = "Thumbnail Card",
+                Text = query.Url,
+                Images = new List<CardImage> { new CardImage("https://raw.githubusercontent.com/microsoft/botframework-sdk/master/icon.png") },
+            };
+
+            var attachments = new MessagingExtensionAttachment(HeroCard.ContentType, null, heroCard);
+            var result = new MessagingExtensionResult(AttachmentLayoutTypes.List, "result", new[] { attachments }, null, "test unfurl");
+
+            return new MessagingExtensionResponse(result);
+        }
+
+
+
+
+        private IActivity CreateActivityWithTextAndSpeak(string message)
+        {
+            var activity = MessageFactory.Text(message);
+            string speak = @"<speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' xml:lang='en-US'>
+              <voice name='Microsoft Server Speech Text to Speech Voice (en-US, JessaRUS)'>" +
+              $"{message}" + "</voice></speak>";
+            activity.Speak = speak;
+            return activity;
+        }
+    }
+}
